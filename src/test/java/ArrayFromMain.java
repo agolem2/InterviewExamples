4150601226ec626179a55a1c25a557862a0bd662
Milo <eronken@hotmail.com>
2024-05-13 07:55:44 -0700

    Create ArrayFromMain.java

---
+++ src/test/java/ArrayFromMain.java
@@ -0,0 +1,13 @@
+package src.test.java;
+
+public class ArrayFromMain {
+    public static int arraylistExample() {
+        int length [] = new int [10];
+
+        length[0] = 2;
+        length[1] = 3;
+        length[5] = 8;
+
+        return length [1];
+    }
+}
